a5b83f089e8b0e236f5012550a8b04bcfad656e4
Yazan Suleiman <yazan.suleiman@gmail.com>
2026-08-09 14:25:29 +0300

    chore(hephaestus): remove unused assigned template type

--- software/modules/hephaestus/src/main/java/totah/lab/hephaestus/receptor/residue/AssignedTemplate.java
+++
@@ -1,9 +0,0 @@
-package totah.lab.hephaestus.receptor.residue;
-
-public record AssignedTemplate(
-        String chainId,
-        int residueNumber,
-        Character insertionCode,
-        String residueName,
-        String templateName) {
-}
